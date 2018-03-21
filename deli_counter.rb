83d71186641b4ccf29a314f631c6189f21f7ef49
Jonathan Williams <jtw365@live.missouristate.edu>
2018-03-21 00:40:15 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
   elsif katz_deli.length >= 1
-    current_line = "The line is currently: "
+    current_line = "The line is currently:"
     katz_deli.each_with_index {|name, index| current_line << " #{index + 1}. #{name}"}
     puts current_line
 
