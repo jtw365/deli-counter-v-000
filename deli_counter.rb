2a4d86296795ba4720499e240c59bcbcc6450cf2
Jonathan Williams <jtw365@live.missouristate.edu>
2018-03-21 01:51:10 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -22,7 +22,7 @@ def now_serving(katz_deli)
     puts "There is nobody waiting to be served!"
   else katz_deli.length >= 1
     serving_line = "Currently serving"
-    katz_deli.each {|name| serving_line << " #{name}"}
+    katz_deli.first {|name| serving_line << " #{name}"}
     puts serving_line
   end
 end
